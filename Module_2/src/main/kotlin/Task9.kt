da9e86c0125982d85f294a14cf111080c2cc6ed4
Dmitriy Vilisov <dvilisov@ibs.ru>
2023-01-26 14:25:42 +0300

    Задание №9

---
+++ Module_2/src/main/kotlin/Task9.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    val countriesWithCapitalAndCurrency  = arrayOf(
+        arrayOf("Китай", "Япония", "Австралия"),
+        arrayOf("Пекин", "Токио", "Канберра"),
+        arrayOf("юань", "иена", "австралийский доллар")
+    );
+
+    for (i in countriesWithCapitalAndCurrency[0].indices) {
+        for (row in countriesWithCapitalAndCurrency) {
+            print("${row[i]}\t");
+        }
+        println();
+    }
+}
