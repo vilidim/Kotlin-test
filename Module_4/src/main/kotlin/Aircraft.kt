0f7805c2f3d6a45b7f08f251e908e7a73a3294fe
Dmitriy Vilisov <dvilisov@ibs.ru>
2023-01-31 13:56:02 +0300

    Задания № 20 - 23

--- Module_4/src/main/kotlin/Aircraft.kt
+++ Module_4/src/main/kotlin/Aircraft.kt
@@ -1,22 +1,49 @@
-open class Aircraft (
+abstract class Aircraft (
     // Номер воздушного судна
-    val number: Int,
+    protected val number: Int,
     // Дальность полёта, км
-    val flightRange: Int,
+    protected val flightRange: Int,
     // Вместимость бака, л
-    val fuelCapacity: Int
+    protected val fuelCapacity: Int
 ) {
 
     // Расход топлива, л / 100 км
-    val fuelConsumption: Double
+    protected val fuelConsumption: Double
     get() = fuelCapacity / flightRange * 100.0;
+
+    open fun printInfo() {
+        println("""
+                   Номер воздушного судна: $number
+                   Дальность полёта: $flightRange км
+                   Вместимость бака: $fuelCapacity л
+                   Расход топлива: $fuelConsumption л / 100 км
+                """.trimIndent());
+    }
+}
+
+interface Passenger {
+    // Кол-во пассажиров
+    val passengersQuantity: Int
 }
 
 class Boeing747 (
     number: Int,
     flightRange: Int,
     fuelCapacity: Int,
-    // Кол-во пассажиров:
-    val passengersQuantity: Int
-) : Aircraft(number, flightRange, fuelCapacity);
+    override val passengersQuantity: Int
+) : Passenger, Aircraft(number, flightRange, fuelCapacity) {
+    override fun printInfo() {
+        super.printInfo();
+        println("Количество пассажиров: $passengersQuantity");
+    }
+}
+
+data class Car(val model: String, val color: String, val vin: String);
+
+fun main(args: Array<String>) {
+    val plane = Boeing747(123, 4000, 50000, 200);
+    plane.printInfo();
+    println();
+    println(Car("Peugeot", "Green", "VFX187467JHGD7466"));
+}
 
